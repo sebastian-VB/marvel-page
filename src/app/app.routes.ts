84205416700c1bc10ce50161c619db109f102223
sebastian-VB <sebas99peru@gmail.com>
2024-03-12 13:13:02 -0500

    home-page: added title with styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './views/components/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePageComponent},
+];
