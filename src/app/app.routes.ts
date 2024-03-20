11ab9595d5e1999e21042aafcc0340c881faa5f6
sebastian-VB <sebas99peru@gmail.com>
2024-03-19 20:41:42 -0500

    home-page.component: added router service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './views/components/home-page/home-page.component';
+import { CharacterPageComponent } from './views/components/character-page/character-page.component';
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent},
+    {path: 'characters', component: CharacterPageComponent}
 ];
--- src/app/views/components/home-page/home-page.component.ts
+++ src/app/views/components/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DividerCardComponent } from '../../../shared/components/divider-card/divider-card.component';
 import { CardDividerInterface } from '../../../shared/models/card-divider.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -15,33 +16,36 @@ export class HomePageComponent {
     {
       id: 1,
       titulo: 'Personajes',
-      ruta: '/characters'
+      ruta: 'characters'
     },
     {
       id: 2,
       titulo: 'Comics',
-      ruta: '/comics'
+      ruta: 'comics'
     },
     {
       id: 3,
       titulo: 'Creadores',
-      ruta: '/creators'
+      ruta: 'creators'
     },
     {
       id: 4,
       titulo: 'Eventos',
-      ruta: '/events'
+      ruta: 'events'
     },
     {
       id: 5,
       titulo: 'Series',
-      ruta: '/series'
+      ruta: 'series'
     }
-  ]
+  ];
+
+  constructor(private router: Router){}
 
   //recibe la ruta y tiene que redirigir
   pathForElement(path: string): void{
     console.log(path);
+    this.router.navigate([path]);
   }
 
 }
