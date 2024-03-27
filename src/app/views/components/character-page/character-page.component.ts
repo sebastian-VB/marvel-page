b428ce638e831f4fe9daf1206e6510c45a25f230
sebastian-VB <sebas99peru@gmail.com>
2024-03-27 17:32:01 -0500

    character-page: added function for list characters

--- src/app/views/components/character-page/character-page.component.ts
+++ src/app/views/components/character-page/character-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, signal } from '@angular/core';
 import { SearchEngineComponent } from '../../../shared/components/search-engine/search-engine.component';
 import { CardElementComponent } from '../../../shared/components/card-element/card-element.component';
 import { CharactersService } from '../../../core/services/characters.service';
@@ -15,13 +15,17 @@ import { FloatingButtonComponent } from '../../../shared/components/floating-but
 export class CharacterPageComponent implements OnInit {
 
   listCharacters: Character[] = [];
+  listCharactersGrowing = signal<Character[]>([]);
+  amountElements: number = 20;
+  sizeList: number = 0;
 
   constructor(private charactersSvc: CharactersService){}
 
   ngOnInit(): void {
     this.charactersSvc.getListCharacter().subscribe((info: ListCharacters) => {
       this.listCharacters = info.data.results;
-      console.log(this.listCharacters);
+      this.listCharactersGrowing.set(this.listCharacters.slice(0, this.amountElements));
+      console.log(this.listCharactersGrowing());
     });
   }
 
@@ -33,8 +37,19 @@ export class CharacterPageComponent implements OnInit {
   //funcion para llenar un array con una cantidad de datos de otro array.
   //usando la funcion array.slice(indice inicio, indice final)
   fillListCharacterForShow(flag: boolean): void{
-    if(flag){
-      console.log("ejecutar funcion")
+    this.amountElements = this.amountElements+20;
+    if(flag && this.amountElements <= this.listCharacters.length){
+      this.sizeList = this.listCharactersGrowing().length;
+      this.listCharactersGrowing.update((characters: Character[]) => {
+        return [
+          ...characters,
+          ...this.listCharacters.slice(this.sizeList, this.amountElements)
+        ]
+      })
+      console.log(this.listCharactersGrowing());
+    }
+    else{
+      this.amountElements = 0;
     }
   }
 
